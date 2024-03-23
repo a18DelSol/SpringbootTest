301f6a7834adf7953de727a61be5fea9d0612527
a18DelSol <juanignacioiglesias00@gmail.com>
2024-03-23 00:58:28 -0300

    initial

---
+++ src/main/java/com/a18delsol/dataneitor9000/Dataneitor9000Application.java
@@ -0,0 +1,13 @@
+package com.a18delsol.dataneitor9000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dataneitor9000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dataneitor9000Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/a18delsol/dataneitor9000/Dataneitor9000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a18delsol.dataneitor9000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dataneitor9000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
