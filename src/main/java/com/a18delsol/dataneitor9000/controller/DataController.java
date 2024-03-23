1363f2f32a0544527e6642d9899308fb3b04af78
a18DelSol <juanignacioiglesias00@gmail.com>
2024-03-23 15:27:07 -0300

    add controller class

---
+++ src/main/java/com/a18delsol/dataneitor9000/controller/DataController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.a18delsol.dataneitor9000.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author heart
+ */
+@RestController
+public class DataController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "it's funny how one verse can fuck up the game";
+    }
+}
